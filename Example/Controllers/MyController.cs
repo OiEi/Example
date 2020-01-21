a2bbebc43b2989477f7e974f5bc3b0e6e735e95a
OiEi <aksenov@smartway.today>
2020-01-21 14:52:23 +0300

    add new method in repo

--- Example/Controllers/MyController.cs
+++ Example/Controllers/MyController.cs
@@ -3,6 +3,8 @@ using Example.Models;
 using System.Collections.Generic;
 using System.Web.Http;
 using System.Linq;
+using System.Net.Http;
+using System.Net;
 
 namespace Example.Controllers
 {
@@ -15,15 +17,28 @@ namespace Example.Controllers
         }
 
         [Route("api/get_employees")]
-        public  IEnumerable<Employee> GetAllEmployee()
+        public IEnumerable<Employee> GetAllEmployee()
         {
             return _repo.GetAll<Employee>();
         }
-               
+
         [Route("api/get_empl/{companyid}")]
         public List<Employee> GetEmplInCompany(int companyid)
         {
             return (from u in _repo.GetAll<Employee>() where u.CompanyId == companyid select u).ToList();
         }
+/*        [Route("api/delete_empl/{employeeid}")]
+        public HttpResponseMessage Delete(int Id)
+        {
+            var employee = (from u in _repo.GetAll<Employee>() where u.id == Id select u);
+            if (employee == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Employee with id {Id} not found");
+            }
+            else
+            {
+                                
+            }
+        }*/
     }
 }
